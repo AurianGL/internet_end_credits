1b7255a0839f6f240c02c70cd91265bf9a180a28
Aurian Guerard des Lauriers <aurian@hotmail.fr>
2020-01-18 18:39:55 +0100

    change title and stuff

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import OpenMenu from './open_menu'
+import {Image, CloudinaryContext} from 'cloudinary-react';
+
 
 export default class Menu extends React.Component {
   state={
@@ -17,11 +19,19 @@ export default class Menu extends React.Component {
       <React.Fragment>
         <div className="win-menu">
           <button 
-            className={this.state.open ? 'win-active' : 'win-start'}
+            className={this.state.open ? 'start win-active' : 'start win-start'}
             onClick={this.onStart}>
+              <CloudinaryContext cloudName="dav38qg9f">
+                <div style={{paddingRight: '3px'}}>
+                  <Image publicId={`Internet_end_credit/icons/star-icon_fvekmm`} width="25" />
+                </div>
+              </CloudinaryContext>
               Start
             </button>
-          <div className='win-clock'>Time</div>
+          <div className='win-clock'>
+            
+            Time
+          </div>
         </div>
         {this.state.open && <OpenMenu/>}
       </React.Fragment>
