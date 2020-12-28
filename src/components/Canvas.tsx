ba128fa2ff4eb139d1e7eb9c502a7eb16d6ad47b
Aurian Guerard <aurian@hotmail.fr>
2020-12-28 17:23:52 +0100

    wiring canvas on local server

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,9 +1,18 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 interface CanvasProps {}
+type Image = {
+  bitmap: {
+    width: number,
+    height: number,
+    data: {
+      data: Uint8ClampedArray
+    }
+  }
+}
+
 
 export const Canvas: React.FC<CanvasProps> = props => {
-  const [deadPixel, setDeadPixel] = useState<number>(0)
 	const canvasRef = useRef<HTMLCanvasElement>(null);
 
 	useEffect(() => {
@@ -13,15 +22,20 @@ export const Canvas: React.FC<CanvasProps> = props => {
 		if (context && canvas) {
       // context.fillStyle = '#000000';
 			// context.fillRect(0, 0, context.canvas.width, context.canvas.height);
-      const imgData = context.getImageData(0, 0, canvas.width, canvas.height)
-      for (let i = 0; i < 100000; i += 4) {
-        imgData.data[i] = 255
-        imgData.data[i + 1] = 255
-        imgData.data[i + 2] = 255
-        imgData.data[i + 3] = 255
-      }
-      console.log(imgData)
-      context.putImageData(imgData, canvas.width, canvas.height)
+      // const imgData = context.getImageData(0, 0, canvas.width, canvas.height)
+      // for (let i = 0; i < 100000; i += 4) {
+      //   imgData.data[i] = 255
+      //   imgData.data[i + 1] = 255
+      //   imgData.data[i + 2] = 255
+      //   imgData.data[i + 3] = 255
+      // }
+      fetch('https://radiant-inlet-32849.herokuapp.com/http://localhost:8000/image')
+        .then(res => res.json())
+        .then((image: Image) => {
+          const {data, width, height} = image.bitmap
+          const imgData = new ImageData(data.data, width, height);
+          context.putImageData(imgData, width, height)
+        })
     }
 	}, []);
 
