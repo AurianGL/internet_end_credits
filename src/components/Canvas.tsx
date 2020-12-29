23e5fa3eee566e7971f19955a1aa3b21caeeebd9
Aurian Guerard <aurian@hotmail.fr>
2020-12-29 10:09:05 +0100

    wired de ramp with API parsing correctly data into canvas image

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -2,41 +2,36 @@ import React, { useEffect, useRef } from 'react';
 
 interface CanvasProps {}
 type Image = {
-  bitmap: {
-    width: number,
-    height: number,
-    data: {
-      data: Uint8ClampedArray
-    }
-  }
-}
-
+	bitmap: {
+		width: number;
+		height: number;
+		data: {
+			data: number[];
+		};
+	};
+};
 
 export const Canvas: React.FC<CanvasProps> = props => {
 	const canvasRef = useRef<HTMLCanvasElement>(null);
 
 	useEffect(() => {
-    const canvas = canvasRef.current
+		const canvas = canvasRef.current;
 		const context = canvas?.getContext('2d');
 
 		if (context && canvas) {
-      // context.fillStyle = '#000000';
-			// context.fillRect(0, 0, context.canvas.width, context.canvas.height);
-      // const imgData = context.getImageData(0, 0, canvas.width, canvas.height)
-      // for (let i = 0; i < 100000; i += 4) {
-      //   imgData.data[i] = 255
-      //   imgData.data[i + 1] = 255
-      //   imgData.data[i + 2] = 255
-      //   imgData.data[i + 3] = 255
-      // }
-      fetch('https://radiant-inlet-32849.herokuapp.com/http://localhost:8000/image')
-        .then(res => res.json())
-        .then((image: Image) => {
-          const {data, width, height} = image.bitmap
-          const imgData = new ImageData(data.data, width, height);
-          context.putImageData(imgData, width, height)
-        })
-    }
+			fetch('http://localhost:8000/image/yeux')
+				.then(res => res.json())
+				.then((image: Image) => {
+					console.log(typeof image.bitmap.data);
+					const { data, width, height } = image.bitmap;
+					const dataImage = context.createImageData(
+						width,
+						height
+          );
+          dataImage.data.set(data.data)
+					context.putImageData(dataImage, 0, 0);
+				});
+		}
 	}, []);
 
 	return <canvas ref={canvasRef} {...props}></canvas>;
