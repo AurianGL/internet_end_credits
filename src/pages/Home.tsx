49e3fc8c77803ce60e4f554b648638d2d5c77f26
AurianGL <aurian.deslauriers@gmail.com>
2021-03-03 21:28:27 +0100

    installed tailwind craco upgrade to CRA 4

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import {
-  Link
-} from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 
-interface HomeProps {
+interface HomeProps {}
 
-}
-
-export const Home: React.FC<HomeProps> = ({}) => {
-    return (
+export const Home: React.FC<HomeProps> = (props) => {
+  return (
+    <div className="w-full h-full">
+      <nav className="text-red-500">
+        <ul  className="flex gap-3 p-2 border-red-500 border-b-2">
+          <li>
+            <Link to="/" className='text-orange-600 bg-blue '>
+              Home
+              </Link>
+          </li>
+          <li>
+            <Link to="/1995">back in 1995</Link>
+          </li>
+          <li>
+            <Link to="/deramp">DE RAMP</Link>
+          </li>
+        </ul>
+      </nav>
       <div>
-        hello world
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/1995">back in 1995</Link>
-            </li>
-            <li>
-              <Link to="/deramp">DE RAMP</Link>
-            </li>
-          </ul>
-        </nav>
+        
       </div>
-    );
-}
+    </div>
+  );
+};
