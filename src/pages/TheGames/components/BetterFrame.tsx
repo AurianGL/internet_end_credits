157300df763f7bc9a99f54dfe2d164e208213545
Aurian Guerard <aurian.guerard@pennylane.com>
2024-08-09 17:48:28 +0200

    the unreachable state

--- src/pages/TheGames/components/BetterFrame.tsx
+++ src/pages/TheGames/components/BetterFrame.tsx
@@ -245,7 +245,7 @@ export const BetterFrame = () => {
           <div className="grid grid-cols-3 grid-rows-3 gap-0">
             <div></div>
             <button
-              className="rounded-t-lg col-start-2 row-start-1 w-12 h-12  bg-transparent bg-gray-400 text-red-500 font-death text-lg"
+              className="select-none rounded-t-lg col-start-2 row-start-1 w-12 h-12  bg-transparent bg-gray-400 text-red-500 font-death text-lg"
               onTouchStart={(e) => handleTouchStart("ArrowUp")}
               onTouchEnd={(e) => handleTouchEnd("ArrowUp")}
             >
@@ -254,18 +254,18 @@ export const BetterFrame = () => {
             <div></div>
 
             <button
-              className="rounded-l-lg col-start-1 row-start-2 w-12 h-12  bg-transparent  bg-gray-400 text-blue-500 font-death text-lg"
+              className="select-none rounded-l-lg col-start-1 row-start-2 w-12 h-12  bg-transparent  bg-gray-400 text-blue-500 font-death text-lg"
               onTouchStart={(e) => handleTouchStart("ArrowLeft")}
               onTouchEnd={(e) => handleTouchEnd("ArrowLeft")}
             >
               Left
             </button>
             <div className="w-12 h-12  bg-transparent bg-gray-400 font-death text-lg" />
-            <button className="rounded-r-lg w-12 h-12  bg-transparent bg-gray-400 text-blue-500 font-death text-lg">
+            <button className="select-none rounded-r-lg w-12 h-12  bg-transparent bg-gray-400 text-blue-500 font-death text-lg">
               Right
             </button>
             <button
-              className="rounded-b-lg col-start-2 row-start-3 w-12 h-12  bg-transparent  bg-gray-400 text-red-500 font-death text-lg"
+              className="select-none rounded-b-lg col-start-2 row-start-3 w-12 h-12  bg-transparent  bg-gray-400 text-red-500 font-death text-lg"
               onTouchStart={(e) => handleTouchStart("ArrowDown")}
               onTouchEnd={(e) => handleTouchEnd("ArrowDown")}
             >
@@ -275,7 +275,7 @@ export const BetterFrame = () => {
           </div>
           <div className="ml-10 grid grid-cols-2 grid-rows-2 gap-0">
             <button
-              className="w-12 h-12 rounded-full bg-red-500 font-death text-lg"
+              className="select-none w-12 h-12 rounded-full bg-red-500 font-death text-lg"
               onTouchStart={(e) => handleTouchStart("e")}
               onTouchEnd={(e) => handleTouchEnd("e")}
             >
@@ -284,7 +284,7 @@ export const BetterFrame = () => {
             <div />
             <div />
             <button
-              className="w-12 h-12 rounded-full bg-blue-500 font-death text-lg"
+              className="select-none w-12 h-12 rounded-full bg-blue-500 font-death text-lg"
               onTouchStart={(e) => handleTouchStart("q")}
               onTouchEnd={(e) => handleTouchEnd("q")}
             >
